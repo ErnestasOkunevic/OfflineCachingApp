d55c6a6e3de663edcb8a4bd0bb565e284f0fff2e
Ernestas <ernestas.okunevic@gmail.com>
2021-06-17 19:23:40 +0300

    add data loading progress bar

--- app/src/main/java/com/ernokun/offlinecachingapp/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/ernokun/offlinecachingapp/ui/feed/FeedFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ernokun.offlinecachingapp.databinding.FragmentFeedBinding
@@ -35,10 +36,27 @@ class FeedFragment : Fragment() {
 
         viewModel.cannabisList.observe(viewLifecycleOwner) { resource ->
             when (resource) {
-                is Resource.Loading -> "Loading..."
-                is Resource.Success -> adapter.submitList(resource.data)
-                is Resource.Error -> "Error: ${resource.message}"
+                is Resource.Loading -> toggleProgressBarVisibility(true)
+                is Resource.Success -> {
+                    toggleProgressBarVisibility(false)
+                    adapter.submitList(resource.data)
+                }
+                is Resource.Error -> {
+                    toggleProgressBarVisibility(false)
+                    showToastMessage("Failed to refresh data: ${resource.message}")
+                }
             }
         }
+    }
+
+    private fun toggleProgressBarVisibility(shouldBeVisible: Boolean) {
+        binding?.progressBar?.visibility = when (shouldBeVisible) {
+            true -> View.VISIBLE
+            else -> View.INVISIBLE
+        }
+    }
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
